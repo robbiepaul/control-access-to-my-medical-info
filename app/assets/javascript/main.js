d1f1c2f69a736cd9e5881ef99fc3687a4a6f4047
Robbie Paul <robbie.paul@dxw.com>
2019-03-25 09:54:00 +0000

    Saturday changes

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,12 @@
 // ES6 or Vanilla JavaScript
+
+$('.context').hide();
+
+$('.nhsuk-radios__input').on('change', function() {
+  var checked = $(this).prop('checked')
+  $('.context').hide();
+  var $context = $(this).parents('.nhsuk-radios__item').find('.context');
+  if (checked) {
+    $context.show();
+  }
+})
